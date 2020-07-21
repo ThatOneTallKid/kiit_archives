259ce0b6fe41fccf931f202c4fa232d5d3e9f003
Rupanshi Chawda <rc80614@gmail.com>
2020-07-21 21:43:44 +0530

    Added Icon and Changed App name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Kiit App',
+      title: 'KiiT Archives',
       debugShowCheckedModeBanner: false,
       home: Home(),
 
