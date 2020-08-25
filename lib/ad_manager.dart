5eb074bfd1b804c38739716bf9ef600a8fcb1a4e
adirups285 <55244625+adirups285@users.noreply.github.com>
2020-08-25 20:26:28 +0530

    commit

---
+++ lib/ad_manager.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class AdManager {
+
+  static String get appId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-4979471076609313~6709284592";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544~2594085930";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get nativeAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-4979471076609313/4909419089";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4339318960";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  
+}
