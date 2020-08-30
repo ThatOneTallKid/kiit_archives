227291c233858a9213d34adfa740e00f74ccfd67
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-08-30 14:22:33 +0530

    chem__egraph updated

---
+++ lib/subjects_year1.dart/E_graphics.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class EGRAPH extends StatefulWidget {
+  @override
+  _EGRAPHState createState() => _EGRAPHState();
+}
+
+class _EGRAPHState extends State<EGRAPH> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Center(
+          child: Text('E.GRAPHICS          '),
+        ),
+        backgroundColor: Color(0xFF06292e),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 4.0, 180.0, 0.0),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side:
+                                BorderSide(color: Color(0xFF06292e), width: 2),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  height: 130,
+                                  width: 150,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child:
+                                        Image.asset('assets/images/books.png'),
+                                  )),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text('SHEETS'),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          launch(
+                              "https://drive.google.com/file/d/1FXuOuJ0g7-NfsSB1QWGw3ui1sgPtgW3k/view?usp=sharing");
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
