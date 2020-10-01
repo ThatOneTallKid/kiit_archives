d3519dff482aa857815cf39cb14543d05a84ef96
Rupanshi Chawda <58527347+rupanshi-chawda@users.noreply.github.com>
2020-10-01 16:16:13 +0530

    Notes and PPT's added in grid.

--- lib/subjects_year2.dart/EE.dart
+++ lib/subjects_year2.dart/EE.dart
@@ -9,11 +9,27 @@ class EE extends StatefulWidget {
 class _EEState extends State<EE> {
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxHeight > 800) {
+        return Layout1();
+      } else {
+        return Layout2();
+      }
+    });
+  }
+}
+
+class Layout1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Center(
-          child: Text('EE               '),
+        title: Text(
+          "Engg. Eco.",
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
         backgroundColor: Color(0xFF06292e),
       ),
@@ -21,7 +37,7 @@ class _EEState extends State<EE> {
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 4.0, 180.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(10.0, 4.0, 8.0, 0.0),
               child: Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -31,8 +47,8 @@ class _EEState extends State<EE> {
                       child: GestureDetector(
                         child: Card(
                           shape: RoundedRectangleBorder(
-                            side:
-                                BorderSide(color: Color(0xFF06292e), width: 2),
+                            side: BorderSide(
+                                color: Color(0xFF66D2AB), width: 1.5),
                             borderRadius: BorderRadius.circular(6),
                           ),
                           child: Column(
@@ -47,21 +63,153 @@ class _EEState extends State<EE> {
                                   )),
                               Padding(
                                 padding: const EdgeInsets.all(4.0),
-                                child: Text('FULL NOTES'),
+                                child: Text('NOTES'),
                               ),
                             ],
                           ),
                         ),
                         onTap: () {
                           launch(
-                              "https://drive.google.com/file/d/17cn-TWIF2RCUXc4srNxG2w6OjaYz-suE/view?usp=sharing");
+                              "https://drive.google.com/drive/folders/1X3mrU4jq42r-Y7TIiiBSmWPHFvlszslc?usp=sharing");
                         },
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 4, 10, 4.0),
+                      child: GestureDetector(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Color(0xFF66D2AB), width: 1.5),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: 130,
+                                width: 150,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.asset('assets/images/books.png'),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('PPTs'),
+                              )
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          launch(
+                              "https://drive.google.com/drive/folders/1FCtSVAD9c9qw_4AWKeul5UOHV84_gktZ?usp=sharing");
+                        },
+                      ),
+                    )
                   ],
                 ),
               ),
             ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Layout2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Engg.Eco.",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color(0xFF06292e),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 4.0, 8.0, 0.0),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Color(0xFF66D2AB), width: 1.5),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  height: 110,
+                                  width: 130,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child:
+                                        Image.asset('assets/images/books.png'),
+                                  )),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text('NOTES'),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          launch(
+                              "https://drive.google.com/drive/folders/1X3mrU4jq42r-Y7TIiiBSmWPHFvlszslc?usp=sharing");
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 4, 10, 4.0),
+                      child: GestureDetector(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Color(0xFF66D2AB), width: 1.5),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: 110,
+                                width: 130,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.asset('assets/images/books.png'),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('PPTs'),
+                              )
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          launch(
+                              "https://drive.google.com/drive/folders/1FCtSVAD9c9qw_4AWKeul5UOHV84_gktZ?usp=sharing");
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            )
           ],
         ),
       ),
