f48bcbed3d92b579a17efc0ae211a10c3b9e0ad7
Alok <kumaralok344@gmail.com>
2021-05-27 21:11:05 +0530

    added default transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:get/get.dart';
+import 'app/global/custom_transition.dart';
 import 'app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:kiit_app/app/services/auth_services.dart';
 
-Future<void> main() async {
+void main() async {
   Get.put(() => AuthService());
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      customTransition: SharedAxisScaleTransition(),
     ),
   );
 }
