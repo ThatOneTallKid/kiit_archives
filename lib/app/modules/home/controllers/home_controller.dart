1abc83cd97c82a08c7682018bcc1eb8d55387f62
namancoder <namanitsofficial@gmail.com>
2021-05-27 19:31:08 +0530

    [v2.0] grid view

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class HomeController extends GetxController {
-
+  ScrollController scrollController = ScrollController();
 }
