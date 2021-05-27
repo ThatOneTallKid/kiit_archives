29ea95dfdc348fa8ae694b0820abf7e32db1e41f
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-05-27 13:54:49 +0530

    [v2.0] login page created

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import 'package:kiit_archives/app/modules/home/bindings/home_binding.dart';
 import 'package:kiit_archives/app/modules/home/views/home_view.dart';
+import 'package:kiit_archives/app/modules/login/bindings/login_binding.dart';
+import 'package:kiit_archives/app/modules/login/views/login_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
